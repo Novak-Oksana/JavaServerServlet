2ff331d655f8a8689dca4786c9187decfbf50812
Novak-Oksana <oksana.babaeva@gmail.com>
2017-10-27 01:37:50 +0300

    init

---
+++ src/com/calc/CalcServlet.java
@@ -0,0 +1,40 @@
+package com.calc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Ksu on 24.10.2017.
+ */
+public class CalcServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        String op = req.getParameter("op");
+        int result = 0;
+        switch(op)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default: throw new UnsupportedOperationException();
+        }
+        PrintWriter out = resp.getWriter();
+        out.print(result);
+    }
+}
